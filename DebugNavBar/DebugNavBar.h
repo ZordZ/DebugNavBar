5ad6976d5d930aeed6d62f3305645d3ba1c05c04
MrTrent <show6time@gmail.com>
2022-08-19 04:50:40 +0300

    Initial project setup

---
+++ DebugNavBar/DebugNavBar.h
@@ -0,0 +1,18 @@
+//
+//  DebugNavBar.h
+//  DebugNavBar
+//
+//  Created by Andrew Zubarew on 19.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DebugNavBar.
+FOUNDATION_EXPORT double DebugNavBarVersionNumber;
+
+//! Project version string for DebugNavBar.
+FOUNDATION_EXPORT const unsigned char DebugNavBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebugNavBar/PublicHeader.h>
+
+
